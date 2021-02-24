f539165be9ce5b24a835e5c05466ae8bf5c019a0
fromlabo <fromlabo@fromlabonoMacBook-Air.local>
2021-02-25 01:07:47 +0900

    secondcommit

---
+++ top.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>課題1-3</title>
+</head>
+<body>
+  <?php
+    $question["問題"] = "日本の首都は？";
+    $answer["回答1"] = "大阪";
+    $answer["回答2"] = "北海道";
+    $answer["回答3"] = "箱根";
+    $answer["回答4"] = "東京";
+  ?>
+  <h1><?php echo '問題　' . $question['問題'] ?><h1>
+  <p><?php
+    foreach($answer as $value) {
+      echo $value . '<br/>';
+    }
+  ?></p>
+  </body>
+</html>
